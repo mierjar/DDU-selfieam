dede3bafa79d28bf9d42f04d497f73846238eda7
mierjar <65968973+mierjar@users.noreply.github.com>
2023-02-10 14:04:03 +0100

    counter

--- sketch.js
+++ sketch.js
@@ -63,11 +63,11 @@ function draw(){
 //en countdown funktion som vil tæller ned med -1 og når den lander på 0 vil den "takesnap"
 function countdown() {
   select('#counter').html(counter)
-  counter = counter - 1
   if(counter == 0) {
     takesnap();
     return;
   } else {
+    counter = counter - 1
     setTimeout(countdown, 1000);
   }
 }
@@ -75,6 +75,7 @@ function countdown() {
 //en takesnap funktion som der vil skjule det live webcam og gemme et screenshot som bliver vist
 function takesnap() {
   videoVisible = false
+  select('#counter').html('')
   image(video,0 , 0, 640, 480);
   setTimeout(showVideo, 5000)
 }
@@ -82,7 +83,6 @@ function takesnap() {
 //en funktion som vil "reset" kameret, den reseter counteren, sætter en background for at skjule det sidste billede og viser webcam igen
 function showVideo() {
   background(0)
-  select('#counter').html('')
   counter = 3
   videoVisible = true;
 
